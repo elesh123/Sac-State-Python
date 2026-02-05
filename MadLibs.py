ad3eac4ad40c8398a96f9f273fa227bf7e770088
elesh123 <elesh904@gmail.com>
2026-02-04 18:05:28 -0800

    Create MadLibs.py
    This is a Madlibs game, where you enter words based on the prompt (ie. adjectives, nouns, verbs),  and it puts them into a story at the end.

---
+++ MadLibs.py
@@ -0,0 +1,136 @@
+print("Welcome to Mad Libs!\nFill in the words below!\n")
+print("")
+
+a=input("Enter an adjective: ")
+print("")
+
+b=input("Enter an adjective: ")
+print("")
+
+c=input("Enter a clothing item: ")
+print("")
+
+d=input("Enter a noun: ")
+print("")
+
+e=d
+
+f=input("Enter an adjective: ")
+print("")
+
+g=input("Enter a food: ")
+print("")
+
+h=input("Enter a plural noun: ")
+print("")
+
+i=input("enter a place: ")
+print("")
+
+j=input("enter an adjective: ")
+print("")
+
+k=input("enter an animal: ")
+print("")
+
+l=input("enter a noun: ")
+print("")
+
+m=input("enter a past tense verb: ")
+print("")
+
+n=input("enter a past tense verb: ")
+print("")
+
+o=input("enter an adjective: ")
+print("")
+
+p=input("enter an adjective: ")
+print("")
+
+q=input("enter a person's name: ")
+print("")
+
+r=input("enter a verb: ")
+print("")
+
+s=input("enter an adjective: ")
+print("")
+
+t=input("enter a noun: ")
+print("")
+
+u=input("enter a past tense verb: ")
+print("")
+
+v=t
+
+w=input("enter a plural noun: ")
+print("")
+
+x=input("enter an adjective: ")
+print("")
+
+y=input("enter an adjective: ")
+print("")
+
+z=input("enter an animal: ")
+print("")
+
+a2=input("enter a verb ending in -ing: ")
+print("")
+
+b2=input("enter a noun: ")
+print("")
+
+c2=input("enter a verb ending in -ing: ")
+print("")
+
+d2=input("enter a plural noun: ")
+print("")
+
+e2=input("enter an adjective: ")
+print("")
+
+f2=input("enter a noun: ")
+print("")
+
+g2=input("enter a place: ")
+print("")
+
+h2=input("enter an adjective: ")
+print("")
+
+i2=input("enter a food: ")
+print("")
+
+j2=input("enter a verb: ")
+print("")
+
+k2=input("enter a plural noun: ")
+print("")
+
+l2=input("enter an adjective: ")
+print("")
+
+print("Your story is ready!")
+print("")
+print("The Day Everything Went Wrong")
+print("Yesterday started off as the", a, "day I had ever experienced.")
+print("I rolled out of bed, put on my", b, c, "and grabbed a", d,"for breakfast.")
+print("Unfortunately, the", e, "was", f,"and tasted like", g, "mixed with", h, ".")
+print("On my way to", i,", I was stopped by a", j, k,"riding a", l,".")
+print("It looked me straight in the eyes and", m, "at me.")
+print("Naturally, I", n, "and screamed, “This is", o, "!“")
+print("When I finally arrived, my", p, q,"told me I had to", r, s, "a", t, u)
+print("before lunch.")
+print("Instead, I accidentally", v, "the", w, ", causing", x, "to fly everywhere.")
+print("Everyone stared at me like I was", y, ".")
+print("Things only got worse.")
+print("A", z, "alarm went off, and a", a2, "began", b2, "across the")
+print("room while holding a", c2, ".")
+print("I tried to stop it by (verb ending in -ing), but slipped on", d2, "and crashed")
+print("into a", e2, f2, ".")
+print("In the end, I escaped to", g2,", where I sat down with a", h2, i2)
+print("and promised myself never to", j2, "again without checking for", k2, "first.")
+print("It was truly the most", l2, "day of my life.")
